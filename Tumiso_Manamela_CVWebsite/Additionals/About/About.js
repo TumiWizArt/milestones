f8caad94e4cb674da5c7f84cfc443eb557d92c68
TumiWizArt <104701155+TumiWizArt@users.noreply.github.com>
2024-08-02 13:25:28 +0200

    Add files via upload

---
+++ Tumiso_Manamela_CVWebsite/Additionals/About/About.js
@@ -0,0 +1,52 @@
+
+var homeLink = document.getElementsByClassName("homeL");
+var portLink = document.getElementsByClassName("portfolioL");
+var aboutLink = document.getElementsByClassName("aboutL");
+var conLink = document.getElementsByClassName("contactL");
+
+
+function settingElements(element){
+    for(var i=0; i<element.length; i++){
+
+        if(element[i] == undefined){
+            continue;
+        }
+        else{
+            return element[i];
+        }
+        
+    }
+}
+
+
+
+
+//settingh the elements
+var changePort = settingElements(portLink);
+var changeHome = settingElements(homeLink);
+var changeContact = settingElements(conLink);
+var changeAbout = settingElements(aboutLink);
+
+
+//seting the colors
+//hover
+function hovering(element){
+    element.addEventListener('mouseover', function(){
+
+        element.style.color = "#78bbeb";
+        changeAbout.style.color = "black";
+        
+ });
+
+
+ element.addEventListener('mouseout', function(){
+
+     element.style.color = "black";
+     changeAbout.style.color = "#78bbeb";
+ });
+}
+
+hovering(changeHome);
+hovering(changePort);
+hovering(changeContact);
+
